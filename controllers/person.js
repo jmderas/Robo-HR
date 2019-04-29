00833c5cf18de4031d920dc430d7789ec2a0d82d
Ricardo Barrera <rbarrera@ptfs.com>
2019-04-29 19:25:45 -0400

    Make tesseract work

--- controllers/person.js
+++ controllers/person.js
@@ -1,14 +1,18 @@
 const Person = require("../models/Person");
-const tesseract = require("tesseract.js");
+const tesseract = require("node-tesseract-ocr");
 
 module.exports = {
   new: function(req, res) {
     //take file from request and run tesseract.
+    //let samplefile = req.files.evaluation;
+    //console.log("sample file " + samplefile);
+    // samplefile.mv(__dirname + '/images/test.jpg', function(err){
+    //   tesseract.recognize(__dirname + '/images/test.jpg').then(function(result) {
+    //     console.log(result);
+    //   });
+    // });
 
-    tesseract.recognize(req.files.evaluation).then(function(result) {
-      console.log(result);
-    });
-
+return;
     //create person object with name, jobTitle, keywords taken from tesseract
   },
   show: function(req, res) {},
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-//const busboy = require("connect-busboy");
+//const busboy = require("connect-busboy");\
+const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const methodOverride = require("method-override");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ extname: ".hbs" });
-const Tesseract = require("tesseract.js");
+//const Tesseract = require("tesseract.js");
 const app = express();
+const tesseract = require("node-tesseract-ocr");
+
 
 app.engine("handlebars", hbs.engine);
 
@@ -13,19 +16,29 @@ app.set("views", __dirname + "/views/");
 app.set("view engine", "hbs");
 app.set("port", process.env.PORT || 4141);
 //app.use(busboy());
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/public", express.static(__dirname + "/public"));
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use(methodOverride("_method"));
 app.use(require("./routes/index"));
 app.use(fileUpload());
-// hbs.registerPartials(__dirname + "/views/partials");
+//hbs.registerPartials(__dirname + "/views/partials");
 app.post("/person/upload", function(req, res) {
-  Tesseract.recognize(req.files.evaluation)
-    .progress(function(message) {
-      console.log("progress is: ", message);
-    })
-    .then(result => console.log(result));
+    //take file from request and run tesseract.
+    let samplefile = req.files.evaluation;
+    console.log("sample file " + samplefile);
+    samplefile.mv(__dirname + '/images/test.jpg', function(err){
+
+		tesseract.recognize(__dirname + '/images/test.jpg', {
+			load_system_dawg: 0,
+			tessedit_char_whitelist: '0123456789',
+			presets: ['hocr', 'digits']
+		}).then(text => {
+			console.log('result:', text)
+		})
+    });
+
+
 });
 app.listen(app.get("port"), () => {
   console.log(`✅ PORT: ${app.get("port")} 🌟`);
